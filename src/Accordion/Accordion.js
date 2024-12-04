f1553f3c5d9a9cb7b870844dcc3223ab1cfd083f
kashifnehal <kashif4918@gmail.com>
2024-12-04 11:00:19 +0530

    added full functionality of only one at a time and double click close

--- src/Accordion/Accordion.js
+++ src/Accordion/Accordion.js
@@ -1,15 +1,30 @@
-import AccordionPanel from "../../src/Accordion/AccordionPanel"
-import { AccordionDetails } from "./constants"
+import AccordionPanel from "../../src/Accordion/AccordionPanel";
+import { AccordionDetails } from "./constants";
+import { useState } from "react";
+
 const Accordion = () => {
-    return(
-        <div> 
-            <header>
-                Accordion
-            </header>
-            <div>
-                {AccordionDetails.map((data, index) => <AccordionPanel data={data} />)}
-            </div>
-        </div>
-    )
-}
-export default Accordion
+  const [activeTab, setActiveTab] = useState(1);
+  const activeTabHandler = (tab) => {
+    if (tab === activeTab) {
+      setActiveTab(null);
+    } else {
+      setActiveTab(tab);
+    }
+  };
+  return (
+    <div>
+      <header>Accordion</header>
+      <div>
+        {AccordionDetails.map((data, index) => (
+          <AccordionPanel
+            data={data}
+            activeTab={activeTab}
+            setActiveTab={(tab) => activeTabHandler(tab)}
+            tabIndex={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Accordion;
--- src/Accordion/AccordionPanel.js
+++ src/Accordion/AccordionPanel.js
@@ -1,19 +1,18 @@
-import { useState } from 'react';
-import './Accordion.css'
+import "./Accordion.css";
 
-const AccordionPanel = ({data}) => {
-    const [showContent, setShowContent] = useState(false)
-    return (
-      <div>
-        <button
-          className="AccordionPanel"
-          onClick={() => setShowContent(!showContent)}
-        >
-          {data.header}
-        </button>
-        {showContent &&  <div>{data.content}</div>}
-      </div>
-    );
-}
+const AccordionPanel = (props) => {
+  const { data, activeTab, setActiveTab, tabIndex } = props;
+  const handleAccordionClick = () => {
+    setActiveTab(tabIndex);
+  };
+  return (
+    <div>
+      <button className="AccordionPanel" onClick={() => handleAccordionClick()}>
+        {data.header}
+      </button>
+      {activeTab === tabIndex && <div>{data.content}</div>}
+    </div>
+  );
+};
 
-export default AccordionPanel
+export default AccordionPanel;
